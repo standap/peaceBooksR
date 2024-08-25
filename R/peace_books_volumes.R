0e70e11732b325b7a7f18001b2855143dc61239d
Standa Pejša <standap@gmail.com>
2024-08-25 13:58:56 -0400

    add data

---
+++ R/peace_books_volumes.R
@@ -0,0 +1,16 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+library(tidyverse)
+peace_books_volumes <- read_csv("data/prothero_text_volumes_1_25_with_economy.csv")
