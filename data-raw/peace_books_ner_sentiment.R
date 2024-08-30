e9a524c97f0964d0219ac2ea710b72cfb8ba393b
Standa Pejša <spejsa@purdue.edu>
2024-08-30 16:05:10 -0400

    adding dataset with sentiment and pos annotation - adding comment

--- data-raw/peace_books_ner_sentiment.R
+++ data-raw/peace_books_ner_sentiment.R
@@ -1,7 +1,10 @@
-## code to prepare `peace_books_ner_sentiment.csv` dataset goes here
-## this fiel was originally called prothero_ser_sentiment.csv
+# code to prepare `peace_books_ner_sentiment.csv` dataset goes here
+# this file was originally called prothero_ser_sentiment.csv
+# the verb lie would only confuse things in corpus - as in this corpus
+# this term is not used in the sense of not telling a truth,
+# but indicating location   filter(!(lemma == "lie" & pos == "VERB"))
 
 library(tidyverse)
-pb_ner_sentiment <- read_csv("data-raw/peace_books_ner_sentiment.csv")
+peace_books_ner_sentiment <- read_csv("data-raw/peace_books_ner_sentiment.csv")
 
 usethis::use_data(pb_ner_sentiment, overwrite = TRUE)
